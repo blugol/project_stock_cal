d95f158e9f60d9480f3d2db1e0ecad0f288f089f
eodnjs4060 <eodnjs4060@gmail.com>
2026-02-01 04:12:51 +0000

    Update project structure and files

---
+++ app/data/uploaded/events.js
@@ -0,0 +1,962 @@
+﻿const __UPLOADED_EVENTS__ = [
+  {
+    "date": "2026-01-01",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 새해",
+        "value": "휴일"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-02",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 시장",
+        "value": "휴일"
+      },
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "ISM 제조업구매자지수  (12월)",
+        "value": "47.948.348.2"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-07",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "22:15",
+        "currency": "USD",
+        "event": "ADP 비농업부문 고용 변화  (12월)",
+        "value": "41K49K-29K"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-08",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "ISM 비제조업구매자지수  (12월)",
+        "value": "54.452.252.6"
+      },
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "미국 노동부 JOLTS (구인, 이직 보고서)  (11월)",
+        "value": "7.146M7.610M7.449M"
+      },
+      {
+        "time": "00:30",
+        "currency": "USD",
+        "event": "원유재고",
+        "value": "-3.832M-1.200M-1.934M"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "신규 실업수당청구건수",
+        "value": "208K213K200K"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-09",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "미국 평균 시간당 임금 (MoM) (12월)",
+        "value": "0.3%0.3%0.2%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "비농업고용지수  (12월)",
+        "value": "50K66K56K"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "실업률  (12월)",
+        "value": "4.4%4.5%4.5%"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-12",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 성인 의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "소비자물가지수 (MoM) (12월)",
+        "value": "0.3%0.3%0.3%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "소비자물가지수 (YoY) (12월)",
+        "value": "2.7%2.7%2.7%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "근원 소비자물가지수 (MoM) (12월)",
+        "value": "0.2%0.3%0.2%"
+      },
+      {
+        "time": "23:59",
+        "currency": "USD",
+        "event": "신규 주택판매  (9월)",
+        "value": "738K711K"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-14",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "신규 주택판매  (10월)",
+        "value": "737K716K738K"
+      },
+      {
+        "time": "04:00",
+        "currency": "USD",
+        "event": "미국 대통령 트럼프가 연설합니다",
+        "value": ""
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 실업률  (12월)",
+        "value": "4.0%2.7%"
+      },
+      {
+        "time": "22:29",
+        "currency": "USD",
+        "event": "생산자물가지수 (MoM) (10월)",
+        "value": "0.1%0.6%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "소매판매 (MoM) (11월)",
+        "value": "0.6%0.5%-0.1%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "근원 소매판매 (MoM) (11월)",
+        "value": "0.5%0.4%0.2%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "생산자물가지수 (MoM) (11월)",
+        "value": "0.2%0.2%0.6%"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-15",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "기존주택판매  (12월)",
+        "value": "4.35M4.21M4.14M"
+      },
+      {
+        "time": "00:30",
+        "currency": "USD",
+        "event": "원유재고",
+        "value": "3.391M-1.700M-3.832M"
+      },
+      {
+        "time": "10:00",
+        "currency": "KRW",
+        "event": "한국 금리 결정  (1월)",
+        "value": "2.50%2.50%2.50%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "필라델피아 연은 제조업활동지수  (1월)",
+        "value": "12.6-1.6-8.8"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "신규 실업수당청구건수",
+        "value": "198K215K207K"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-19",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 마틴루터킹 쥬니어 추모일",
+        "value": "휴일"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-21",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "미국 대통령 트럼프가 연설합니다",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-01-22",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 GDP (QoQ) (4분기)",
+        "value": "-0.3%0.1%1.3%"
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 GDP (YoY) (4분기)",
+        "value": "1.5%1.9%1.8%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "GDP (QoQ) (3분기)",
+        "value": "4.4%4.3%3.8%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "신규 실업수당청구건수",
+        "value": "200K209K199K"
+      },
+      {
+        "time": "23:59",
+        "currency": "USD",
+        "event": "근원 소비지출물가지수 (MoM) (10월)",
+        "value": "0.2%0.2%"
+      },
+      {
+        "time": "23:59",
+        "currency": "USD",
+        "event": "근원 개인소비지출 물가지수 (YoY) (10월)",
+        "value": "2.7%2.8%"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-23",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "근원 소비지출물가지수 (MoM) (11월)",
+        "value": "0.2%0.2%0.2%"
+      },
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "근원 개인소비지출 물가지수 (YoY) (11월)",
+        "value": "2.8%2.8%2.7%"
+      },
+      {
+        "time": "02:00",
+        "currency": "USD",
+        "event": "원유재고",
+        "value": "3.602M-1.000M3.391M"
+      },
+      {
+        "time": "12:00",
+        "currency": "JPY",
+        "event": "금리결정",
+        "value": "0.75%0.75%0.75%"
+      },
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "서비스 구매관리자지수  (1월)",
+        "value": "52.552.952.5"
+      },
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "제조업 구매관리자지수  (1월)",
+        "value": "51.951.951.8"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-28",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "CB 소비자신뢰지수  (1월)",
+        "value": "84.590.694.2"
+      },
+      {
+        "time": "06:00",
+        "currency": "USD",
+        "event": "미국 대통령 트럼프가 연설합니다",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-01-29",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "00:30",
+        "currency": "USD",
+        "event": "원유재고",
+        "value": "-2.295M-0.200M3.602M"
+      },
+      {
+        "time": "04:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": "3.75%3.75%3.75%"
+      },
+      {
+        "time": "04:00",
+        "currency": "USD",
+        "event": "연방공개시장위원회 성명서",
+        "value": ""
+      },
+      {
+        "time": "04:30",
+        "currency": "USD",
+        "event": "FOMC 기자 회견",
+        "value": ""
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "신규 실업수당청구건수",
+        "value": "209K206K210K"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-30",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "06:30",
+        "currency": "USD",
+        "event": "미국 대통령 트럼프가 연설합니다",
+        "value": ""
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 소매판매 (MoM) (12월)",
+        "value": "0.9%-3.3%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "생산자물가지수 (MoM) (12월)",
+        "value": "0.5%0.2%0.2%"
+      }
+    ]
+  },
+  {
+    "date": "2026-01-31",
+    "day": "토요일",
+    "schedules": [
+      {
+        "time": "10:30",
+        "currency": "CNY",
+        "event": "중국 제조업 구매관리자지수  (1월)",
+        "value": "49.350.150.1"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-02",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "제조업 구매관리자지수  (1월)",
+        "value": "51.951.8"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-03",
+    "day": "화요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "ISM 제조업구매자지수  (1월)",
+        "value": "47.9"
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 소비자물가지수 (YoY) (1월)",
+        "value": "2.3%"
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 소비자물가지수 (MoM) (1월)",
+        "value": "0.3%"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-04",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "미국 노동부 JOLTS (구인, 이직 보고서)  (12월)",
+        "value": "7.146M"
+      },
+      {
+        "time": "22:15",
+        "currency": "USD",
+        "event": "ADP 비농업부문 고용 변화  (1월)",
+        "value": "41K"
+      },
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "서비스 구매관리자지수  (1월)",
+        "value": "52.552.5"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-05",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "ISM 비제조업구매자지수  (1월)",
+        "value": "54.4"
+      },
+      {
+        "time": "00:30",
+        "currency": "USD",
+        "event": "원유재고",
+        "value": "-2.295M"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "신규 실업수당청구건수",
+        "value": "209K"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-06",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "미국 평균 시간당 임금 (MoM) (1월)",
+        "value": "0.3%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "비농업고용지수  (1월)",
+        "value": "50K"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "실업률  (1월)",
+        "value": "4.4%"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-11",
+    "day": "수요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 창립 국경일",
+        "value": "휴일"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "소비자물가지수 (MoM) (1월)",
+        "value": "0.3%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "소비자물가지수 (YoY) (1월)",
+        "value": "2.7%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "근원 소비자물가지수 (MoM) (1월)",
+        "value": "0.2%"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-13",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "기존주택판매  (1월)",
+        "value": "4.35M"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-16",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 워싱턴 탄생일",
+        "value": "휴일"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-17",
+    "day": "화요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 중국 춘절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 중국 춘절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 중국 춘절",
+        "value": "휴일"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-20",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 중국 춘절",
+        "value": "휴일"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "GDP (QoQ) (4분기)",
+        "value": "4.4%"
+      },
+      {
+        "time": "22:30",
+        "currency": "USD",
+        "event": "근원 개인소비지출 물가지수 (YoY) (12월)",
+        "value": "2.8%"
+      },
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "제조업 구매관리자지수  (2월)",
+        "value": "51.8"
+      },
+      {
+        "time": "23:45",
+        "currency": "USD",
+        "event": "서비스 구매관리자지수  (2월)",
+        "value": "52.5"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-21",
+    "day": "토요일",
+    "schedules": [
+      {
+        "time": "00:00",
+        "currency": "USD",
+        "event": "신규 주택판매  (11월)",
+        "value": "737K"
+      }
+    ]
+  },
+  {
+    "date": "2026-02-23",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 일왕 생일",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "한국 - 삼일절 대체공",
+        "value": "휴일"
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 GDP (QoQ) (4분기)",
+        "value": "-0.3%"
+      },
+      {
+        "time": "08:00",
+        "currency": "KRW",
+        "event": "한국 GDP (YoY) (4분기)",
+        "value": "1.5%"
+      }
+    ]
+  },
+  {
+    "date": "2026-03-19",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": "3.75%"
+      }
+    ]
+  },
+  {
+    "date": "2026-03-20",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 버날 이퀴녹스",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 부활절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 청명절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 쇼와의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-05-01",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 근로자의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 일본의 녹색의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 어린이 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 제헌절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 현충일",
+        "value": "휴일"
+      },
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-06-19",
+    "day": "금요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - Juneteenth",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 삼일절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 해양의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-08-11",
+    "day": "화요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 산의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "한국 - 광복절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 근로자의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-09-21",
+    "day": "월요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 일본 경로의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 -",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 추분절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "한국 - 추석",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 중추절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 개천절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 개천절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 개천절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 개천절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "중국 - 개천절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "한국 - 한글날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - Sport Day",
+        "value": "휴일"
+      },
+      {
+        "time": "03:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-11-03",
+    "day": "화요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 문화의 날",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 노동 감사절",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 추수감사절",
+        "value": "휴일"
+      },
+      {
+        "time": "04:00",
+        "currency": "USD",
+        "event": "금리결정",
+        "value": ""
+      }
+    ]
+  },
+  {
+    "date": "2026-12-24",
+    "day": "목요일",
+    "schedules": [
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "미국 - 크리스마스",
+        "value": "휴일"
+      },
+      {
+        "time": "하루 종일",
+        "currency": "",
+        "event": "일본 - 시장",
+        "value": "휴일"
+      }
+    ]
+  }
+];
